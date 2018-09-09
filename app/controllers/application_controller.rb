7bd34098cefeccdf345e31cd3b2b5c915ae3d550
John S <diaz.john312@gmail.com>
2018-09-09 10:02:30 -0400

    Minor tweaks to application_controller helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
                 :logged_in?,
                 :current_user_is_admin,
                 :readable_role,
-                :same_user,
+                :same_user?,
                 :we_can_edit
 
   def current_user
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
-  def same_user(id)
+  def same_user?(id)
     return false if !logged_in?
 
     current_user.id === id
@@ -29,17 +29,18 @@ class ApplicationController < ActionController::Base
   def we_can_edit(user)
     return false if !logged_in?
 
-    same_user(user.id) || (current_user_is_admin && user.role != 1) 
+    we_are_same_user = same_user?(user.id)
+    we_have_admin_permission_to_edit = current_user_is_admin && user.role != 1 # admins cannot edit other admins
+
+    we_are_same_user or we_have_admin_permission_to_edit 
   end
   def readable_role(number)
     number = number.to_i
-    case
-    when number == 1
+    case number
+    when 1
       return 'Admin'
-    when number == 2
+    when 2
       return 'Superintendent'
-    when number ==3
-      return 'Subcontractor'
     end
   end
 end
