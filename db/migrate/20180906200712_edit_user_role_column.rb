f077773beedd9e77ff86d440aeae83c302549327
John S <diaz.john312@gmail.com>
2018-09-09 21:08:32 -0400

    role as integer

--- db/migrate/20180906200712_edit_user_role_column.rb
+++ db/migrate/20180906200712_edit_user_role_column.rb
@@ -1,5 +1,5 @@
 class EditUserRoleColumn < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :role, :integer, 'integer USING CAST(incident_id AS integer)'
+    change_column :users, :role, :integer, 'integer USING CAST(role AS integer)'
   end
 end
