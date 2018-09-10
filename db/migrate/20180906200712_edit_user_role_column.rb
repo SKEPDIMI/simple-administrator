1ceda04559592acbd56ca70b170a24bf9bf9075c
John S <diaz.john312@gmail.com>
2018-09-09 21:06:40 -0400

    migration convert string to integer using cast

--- db/migrate/20180906200712_edit_user_role_column.rb
+++ db/migrate/20180906200712_edit_user_role_column.rb
@@ -1,5 +1,5 @@
 class EditUserRoleColumn < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :role, :integer
+    change_column :users, :role, :integer, 'integer USING CAST(incident_id AS integer)'
   end
 end
